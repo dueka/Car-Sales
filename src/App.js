e626a7e0180c96dfb5bbe4c33dcd6a916cd46e9d
dueka <ekanemd@live.com>
2019-12-11 17:34:44 +0100

    added the monsterReducer and the import

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import AddedFeatures from './components/AddedFeatures';
-import AdditionalFeatures from './components/AdditionalFeatures';
-import Total from './components/Total';
+import Header from "./components/Header";
+import AddedFeatures from "./components/AddedFeatures";
+import AdditionalFeatures from "./components/AdditionalFeatures";
+import Total from "./components/Total";
+import { combineReducers, createStore } from "redux";
+import { addReducer } from "../src/components/state/reducers";
+import { from } from "rxjs";
+
+const monsterReducer = combineReducers({
+  add: addReducer
+});
+
+const store = createStore(
+  monsterReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: '2019 Ford Mustang',
-      image:
-        'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
-      features: []
-    },
-    additionalFeatures: [
-      { id: 1, name: 'V-6 engine', price: 1500 },
-      { id: 2, name: 'Racing detail package', price: 1500 },
-      { id: 3, name: 'Premium sound system', price: 500 },
-      { id: 4, name: 'Rear spoiler', price: 250 }
-    ]
-  };
-
   const removeFeature = item => {
     // dispatch an action here to remove an item
   };
