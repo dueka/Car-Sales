01b42fff1ef0a6801ed241662e37f0ea37432d06
dueka <ekanemd@live.com>
2019-12-11 17:01:35 +0100

    added the reducer

---
+++ src/components/state/reducers.js
@@ -0,0 +1,41 @@
+import * as types from "./actionTypes";
+
+// const state = {
+//     additionalPrice: 0,
+//     car: {
+//       price: 26395,
+//       name: '2019 Ford Mustang',
+//       image:
+//         'https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg',
+//       features: []
+//     },
+// additionalFeatures: [
+//   { id: 1, name: "V-6 engine", price: 1500 },
+//   { id: 2, name: "Racing detail package", price: 1500 },
+//   { id: 3, name: "Premium sound system", price: 500 },
+//   { id: 4, name: "Rear spoiler", price: 250 }
+// ];
+
+const additionalPrice = 0;
+const additionalFeatures = [
+  { id: 1, name: "V-6 engine", price: 1500 },
+  { id: 2, name: "Racing detail package", price: 1500 },
+  { id: 3, name: "Premium sound system", price: 500 },
+  { id: 4, name: "Rear spoiler", price: 250 }
+];
+export function addReducer(
+  features = additionalFeatures,
+  count = additionalPrice,
+  action
+) {
+  switch (action.type) {
+    case types.ADD:
+      return features.map(item => {
+        return item.id === action.payload.id;
+      });
+    case types.REMOVE:
+      return count - 1;
+    default:
+      return count;
+  }
+}
